acdbf4c73c3e6454754a7dcf14a683781847ce39
mfDK <dkim7474@gmail.com>
2015-11-08 04:05:08 -0500

    update README

---
+++ js/grid.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    var box = $('.box');
+    
+    for (var i = 1; i < 16; i++) {
+    	$('.wrapper').append(box.clone());
+    };
+    
+    var column = $('.wrapper');
+    for (var n = 1; n < 16; n++) {
+    	column.after(column.clone());
+    }
+    
+    $('.box').hover(function() {
+    	$(this).css("background-color", "#2ECCFA")
+    }, function() {
+    	$(this).css("background-color", "#DA81F5")
+    });
+
+    
+    
+    
+});
