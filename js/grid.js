e07c1de3620616d4a01e87c8d69d4cc475c507b2
mfDK <dkim7474@gmail.com>
2015-11-08 05:35:50 -0500

    update README

--- js/grid.js
+++ js/grid.js
@@ -2,21 +2,23 @@ $(document).ready(function() {
     var box = $('.box');
     
     for (var i = 1; i < 16; i++) {
-    	$('.wrapper').append(box.clone());
+        $('.wrapper').append(box.clone());
     };
     
     var column = $('.wrapper');
     for (var n = 1; n < 16; n++) {
-    	column.after(column.clone());
+        column.after(column.clone());
     }
     
     $('.box').hover(function() {
-    	$(this).css("background-color", "#2ECCFA")
+        $(this).css("background-color", "#2ECCFA")
     }, function() {
-    	$(this).css("background-color", "#DA81F5")
+        $(this).css("background-color", "#DA81F5")
     });
-
     
     
+    $('button').on('click', function() {
+        $('.box').css("background-color", "#FFF");
+    });
     
 });
