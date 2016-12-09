8e21ceedd667286092af8070746bce559ac85ea2
Danny Kim <dkim7474@gmail.com>
2016-12-08 19:58:56 -0500

    Found a function to shuffle items in an array

--- js/grid.js
+++ js/grid.js
@@ -21,6 +21,29 @@ $(document).ready(function() {
         $('.box').css("background-color", "#FFF");
     });
     
+
 });
 
-/*hello test*/
+function changeColor() {
+    var digits = [1,2,3,4,5,6,7,8,9,0,"A", "B", "C", "E", "F"];
+        hexLetter = [];
+}
+
+// FISHER-Yates (AKA Knuth) Shuffle
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle
+    while (0 !== currentIndex) {
+        // Pick a remaining element
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // and swap it with the current element
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
